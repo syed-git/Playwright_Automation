5f214d7ad79063f89bd32dd37a52d7588b1e3ac5
Abu Bakar Syed <sdzubair.engg@gmail.com>
2025-02-02 16:20:05 -0600

    updated tests

--- tests/UI/regression/invalid-login.spec.ts
+++ tests/UI/regression/invalid-login.spec.ts
@@ -11,8 +11,7 @@ test('Verify Error message for invalid login attempt - #error #regression', asyn
 
     const pageActions = new PageActionsHelper(page);
     const pageValidations = new PageValidationsHelper(page);
-    const loginHelper = new LoginHelper(page);
-
+    
     await pageActions.navigateTo(env.sit.baseUrl);
     await pageValidations.seeTitleContains(homePage.pageTitle, true);
     await pageValidations.seeElementExists(homePage.home);
@@ -23,4 +22,22 @@ test('Verify Error message for invalid login attempt - #error #regression', asyn
     await pageActions.enterPassword(signUp.loginPassword, '67Fghy#-==');
     await pageActions.clickOn(signUp.loginButton);
     await pageValidations.seeElementContains(signUp.errorMessage, 'Your email or password is incorrect!', true);
+});
+
+test('Verify Error message when resgistering with existing email - #error #regression', async ({ page }) => {
+
+    const pageActions = new PageActionsHelper(page);
+    const pageValidations = new PageValidationsHelper(page);
+
+    await pageActions.navigateTo(env.sit.baseUrl);
+    await pageValidations.seeTitleContains(homePage.pageTitle);
+    await pageValidations.seeElementExists(homePage.home);
+    await pageActions.clickOn(homePage.signUpButton);
+    await pageValidations.seeTitleContains(signUp.pageTitle, true);
+    await pageValidations.seeElementPresent(signUp.newUserSignUp, 'visible');
+    await pageValidations.seeElementExists(signUp.loginToYourAccount);
+    await pageActions.fillField(signUp.name, env.sit.register.name);
+    await pageActions.fillField(signUp.signUpEmail, `syed.zubair4929@gmail.com`);
+    await pageActions.clickOn(signUp.signUpButton);
+    await pageValidations.seeElementContains(signUp.existingEmailError, 'Email Address already exist!', true);
 });
